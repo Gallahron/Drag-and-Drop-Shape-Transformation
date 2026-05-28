5899db055ca1c7f6d5cdfa26e145f0c7a4c232f1
Gallahron <=>
2026-05-28 22:35:00 +0100

    Add keyboard controls, add missing shape

--- index.js
+++ index.js
@@ -8,8 +8,11 @@
         'hexagon': 'resources/shape-6-sided-hexagon.svg'
     }
 
+    const quadrants = [... document.getElementsByClassName("quadrant")];
+    const shapes = [... document.getElementsByClassName("shape")];
+
     init = () => {
-        [... document.getElementsByClassName("shape")].forEach(shape => {
+        shapes.forEach(shape => {
             shape.addEventListener("dragstart", e => {
                 activeDragShape = e.target;
             });
@@ -17,7 +20,7 @@
             updateShape(shape, shape.parentNode.dataset.targetShape);
         });
         
-        [... document.getElementsByClassName("quadrant")].forEach(quadrant => {
+        quadrants.forEach(quadrant => {
             quadrant.addEventListener("dragover", e => {
                 e.preventDefault();
             });
@@ -25,12 +28,41 @@
             quadrant.addEventListener("drop", e => {
                 e.preventDefault();
 
-                activeDragShape.parentNode.removeChild(activeDragShape);
-                e.target.appendChild(activeDragShape);
-
-                updateShape(activeDragShape, e.target.dataset.targetShape);
+                moveShape(e.target);
             })
         });
+
+        document.addEventListener("keydown", e => {
+            if (e.key === " ") {
+                if (activeDragShape == null && document.activeElement.classList.contains("shape")) {
+                    activeDragShape = document.activeElement;
+                    activeDragShape.classList.add("highlighted");
+                    makeQuadrantsTabbable();
+                }
+                else if (document.activeElement.classList.contains("quadrant")) {
+                    activeDragShape.classList.remove("highlighted");
+                    moveShape(document.activeElement);
+                    makeShapesTabbable();
+                }
+            }
+            else if (e.key === "Escape") {
+                activeDragShape.classList.remove("highlighted");
+                activeDragShape = null;
+                makeShapesTabbable();
+            }
+        });
+
+        makeShapesTabbable();
+    }
+
+    moveShape = (target) => {
+        activeDragShape.parentNode.removeChild(activeDragShape);
+        target.appendChild(activeDragShape);
+
+        updateShape(activeDragShape, target.dataset.targetShape);
+        makeShapesTabbable();
+
+        activeDragShape = null;
     }
 
     updateShape = (element, targetShape) => {
@@ -38,6 +70,28 @@
 
         element.src = shapeDisplays[targetShape];
     };
+
+
+    makeShapesTabbable = () => {
+        let i = 1;
+
+        quadrants.forEach(quadrant => {
+            quadrant.setAttribute("tabindex", -1);
+
+            [...quadrant.getElementsByClassName("shape")].forEach(shape => {
+                shape.setAttribute("tabindex", i++);
+            });
+        });
+
+        document.activeElement.blur();
+    }
+
+    makeQuadrantsTabbable = () => {
+        shapes.forEach(shape => shape.setAttribute("tabindex", -1));
+        
+        let i = 1;
+        quadrants.forEach(quadrant => quadrant.setAttribute("tabindex", i++));
+    }
 
     init();
 })()
