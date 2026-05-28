e2da3b5b88d4a50b4e0e026e12cc8a997d41da89
Gallahron <=>
2026-05-28 21:50:47 +0100

    Add the ability to move shapes between quadrants; update shape display method to improve drag display

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 (() => {
+    let activeDragShape = null;
+
     init = () => {
-    
+        [... document.getElementsByClassName("shape")].forEach(shape => {
+            shape.addEventListener("dragstart", e => {
+                activeDragShape = e.target;
+            })
+        });
+        
+        [... document.getElementsByClassName("quadrant")].forEach(quadrant => {
+            quadrant.addEventListener("dragover", e => {
+                e.preventDefault();
+            });
+
+            quadrant.addEventListener("drop", e => {
+                e.preventDefault();
+
+                activeDragShape.parentNode.removeChild(activeDragShape);
+                e.target.appendChild(activeDragShape);
+            })
+        });
     }
 
     init();
