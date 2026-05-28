19a163bd920eef44184b4089cb8b5e076e40a4b9
Gallahron <=>
2026-05-28 21:56:45 +0100

    Implement shape changing

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 (() => {
     let activeDragShape = null;
 
+    const shapeDisplays = {
+        'square': 'resources/shape-4-sided-square.svg',
+        'circle': 'resources/circle.svg',
+        'triangle': 'resources/shape-3-sided-triangle.svg',
+        'hexagon': 'resources/shape-6-sided-hexagon.svg'
+    }
+
     init = () => {
         [... document.getElementsByClassName("shape")].forEach(shape => {
             shape.addEventListener("dragstart", e => {
                 activeDragShape = e.target;
-            })
+            });
+
+            updateShape(shape, shape.parentNode.dataset.targetShape);
         });
         
         [... document.getElementsByClassName("quadrant")].forEach(quadrant => {
@@ -18,9 +27,17 @@
 
                 activeDragShape.parentNode.removeChild(activeDragShape);
                 e.target.appendChild(activeDragShape);
+
+                updateShape(activeDragShape, e.target.dataset.targetShape);
             })
         });
     }
+
+    updateShape = (element, targetShape) => {
+        if (shapeDisplays[targetShape] == undefined) return;
+
+        element.src = shapeDisplays[targetShape];
+    };
 
     init();
 })()
